83a183d763e2602a3f1d406dee0d9011951a00d8
sczternasty <sczternasty@gmail.com>
2024-03-25 18:41:16 +0100

    .

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ while not(os.path.isfile(os.path.join(os.getcwd(), ontology_name))):
     print("Error: File not found")
     ontology_name = input("Ontology name: ")
 concept_name = input("Concept name (if necessary add quotation marks): ")
-max_iter = int(input("Maximum number of iterations: "))
+max_depth = int(input("Maximum depth: "))
 
 axioms = loadOntology(ontology_name)
-complex_concept = unfold_tbox(concept_name, axioms, max_iter)
+complex_concept = unfold_tbox(concept_name, axioms, max_depth)
 ABox_dict = tableauMethod(complex_concept)
 graph = generate_graph(ABox_dict)
 graph.render(concept_name + " graph", format='png', cleanup=True)
